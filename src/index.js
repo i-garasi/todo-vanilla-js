287bd71932d0eca5299cc6284165e262cb3b41ab
i-garasi <44829724+i-garasi@users.noreply.github.com>
2021-04-12 00:05:45 +0900

    update: todoの追加機能

--- src/index.js
+++ src/index.js
@@ -10,7 +10,21 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "削除";
+  removeButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(removeButton);
 
   document.getElementById("incomplete-list").appendChild(div);
 };
